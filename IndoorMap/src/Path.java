d03b0c1a00c7f4c5ea613b86bf67916f5a1bbb66
kjwalters1 <walters.792@osu.edu>
2023-10-28 14:10:41 -0400

    Added Method shells

--- IndoorMap/src/Path.java
+++ IndoorMap/src/Path.java
@@ -5,4 +5,16 @@ public class Path {
     public Path(Location start, Location finish, IndoorMap im) {
 
     }
+
+    public static void buildPath() {
+
+    }
+
+    public static void start() {
+
+    }
+
+    public static void next() {
+
+    }
 }
